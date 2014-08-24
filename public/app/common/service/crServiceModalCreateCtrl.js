9bea1b1f790312c5b8433a672f1cc2ac03939c88
Hung Ho <ho.ngoc.hung2812@gmail.com>
2014-08-24 17:09:59 +0300

    Remove 2 redundant files

--- public/app/common/service/crServiceModalCreateCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('app').controller('crServiceModalCreateCtrl',
-            function($scope, $http, $location, crNotifier){
-
-    $scope.createNewService = function(){
-        $scope.services.push($scope.service_name);
-        crNotifier.notify('New person has been created!');
-    };
-});
